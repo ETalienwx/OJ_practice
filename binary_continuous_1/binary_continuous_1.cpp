b111c1087ad9a8abca4fc284709cba8f373a6423
wangxuan <wangxuan0809@126.com>
2019-06-19 18:12:41 +0800

    binary_continuous_1

---
+++ binary_continuous_1/binary_continuous_1.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main()
+{
+    int n;
+    while(cin>>n)
+    {
+        int count=0;
+        int maxcount=0;
+        while(n)
+        {
+            if(n&1==1)
+            {
+                count++;
+                maxcount=max(count,maxcount);
+            }
+            else
+                count=0;
+            n=n>>1;
+        }
+        cout<<maxcount<<endl;
+    }
+    return 0;
+}
