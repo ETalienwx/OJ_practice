fdece2f4e24139192eebfca61e110c3a16133e32
wangxuan <wangxuan0809@126.com>
2019-06-29 11:07:02 +0800

    string_inversion

---
+++ string_inversion/string_inversion.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+int main()
+{
+    string str;
+    while(cin>>str)
+    {
+        reverse(str.begin(),str.end());
+        cout<<str<<endl;
+    }
+    system("pause");
+    return 0;
+}
