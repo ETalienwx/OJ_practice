8953b77a81c397ac7c63e18ba05ff44795e3bd8a
wangxuan <wangxuan0809@126.com>
2019-07-20 21:47:00 +0800

    original_text_and_password

---
+++ original_text_and_password/original_text_and_password.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main() {
+	char c;
+	while ((c = getchar()) != EOF){
+		if ('A' <= c && 'Z' >= c) {
+			c = (c > 'E') ? (c - 5) : (c + 21);
+		} 
+		putchar(c);
+	} 
+	return 0;
+}
